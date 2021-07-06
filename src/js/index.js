c740140d917b5ba6d171f4749cac8ab545cb9953
Sereda Vladyslav <v.u.sereda@gmail.com>
2021-07-06 16:19:50 +0300

    fix by remarks

--- src/js/index.js
+++ src/js/index.js
@@ -1,70 +1,56 @@
 console.log('------------- #4')
 function sum(a, b) {
-    const result = a + b
-    console.log(result)
+    return a + b
 }
-sum(2,6)
 
 function minus(a, b) {
-    const result = a - b
-    console.log(result)
+    return a - b
 }
-minus(8,6)
 
 function times(a, b) {
-    const result = a * b
-    console.log(result)
+    return a * b
 }
-times(2,6)
 
 function division(a,b) {
-    const result = a / b
-    console.log(result)
+    return a / b
 }
-division(12,6)
+
+console.log(sum(2,6))
+console.log(minus(8,6))
+console.log(times(2,6))
+console.log(division(12,6))
 
 console.log('------------- #5')
 function fromOneToEnterValue(val) {
-    let arr = [];
     for (let i = 1; i <= val; i++) {
-        arr.push(i);
+        console.log(i)
     }
-    console.log(arr.join(' '))
 }
 fromOneToEnterValue(5)
 
 console.log('------------- #6')
 function fromEnterValueToOne(val) {
-    let arr = [];
-    /*for (let i = 1; i <= val; i++) {
-        arr.push(i);
-    }
-    console.log(arr.reverse().join(' '))*/
     let i = 0
     for (val; i < val; val--) {
-        arr.push(val);
+        console.log(val)
     }
-    console.log(arr.join(' '))
 }
 fromEnterValueToOne(5)
 
 console.log('------------- #7')
 function pow(a,b) {
-    const result = a ** b
-    console.log(result)
+    return a ** b
 }
-pow(2,3)
+console.log(pow(2,3))
 
 console.log('------------- #8')
 function isBigger(a, b) {
-    const result = a > b
-    console.log(result)
+    return a > b
 }
-isBigger(5, -1);
+console.log(isBigger(5, -1));
 
 console.log('------------- #9')
 function isSmaller(a, b) {
-    const result = a < b
-    console.log(result)
+    return a < b
 }
-isSmaller(5, -1);
+console.log(isSmaller(5, -1));
